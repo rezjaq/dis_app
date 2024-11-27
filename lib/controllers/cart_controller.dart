d24a63ba18e01f15db57c92e928925c1e546f2e7
fakhruddinarif <muhfakhruddinarif30@gmail.com>
2024-11-27 07:31:09 +0700

    modify: model and cart controller

--- lib/controllers/cart_controller.dart
+++ lib/controllers/cart_controller.dart
@@ -3,7 +3,7 @@ import 'package:dis_app/utils/http/http_client.dart';
 import 'package:dis_app/models/cart_model.dart';
 
 class CartController {
-  Future<Map<String, dynamic>> addToCart(AddToCartRequest request) async {
+  Future<Map<String, dynamic>> addToCart(AddItemRequest request) async {
     try {
       final response = await DisHttpClient.post('cart', request.toJson());
       if (response['data'] == null) {
@@ -16,9 +16,9 @@ class CartController {
     }
   }
 
-  Future<Map<String, dynamic>> removeFromCart(RemoveFromCartRequest request) async {
+  Future<Map<String, dynamic>> removeFromCart(RemoveItemRequest request) async {
     try {
-      final response = await DisHttpClient.delete('cart/${request.id}');
+      final response = await DisHttpClient.delete('cart/${request.photoId}');
       if (response['data'] == null) {
         throw response['errors'];
       } else {
@@ -29,33 +29,7 @@ class CartController {
     }
   }
 
-  Future<Map<String, dynamic>> updateCart(UpdateCartRequest request) async {
-    try {
-      final response = await DisHttpClient.patch('cart/${request.id}', request.toJson());
-      if (response['data'] == null) {
-        throw response['errors'];
-      } else {
-        return response['data'];
-      }
-    } catch (e) {
-      throw Exception('Failed to update cart: $e');
-    }
-  }
-
-  Future<Map<String, dynamic>> getCart(GetCartRequest request) async {
-    try {
-      final response = await DisHttpClient.get('cart/${request.userId}');
-      if (response['data'] == null) {
-        throw response['errors'];
-      } else {
-        return response['data'];
-      }
-    } catch (e) {
-      throw Exception('Failed to get cart: $e');
-    }
-  }
-
-  Future<Map<String, dynamic>> listCartItems(ListCartRequest request) async {
+  Future<Map<String, dynamic>> listCartItems(ListItemsRequest request) async {
     try {
       final queryParams = request.toQueryParams();
       final response = await DisHttpClient.get('cart?$queryParams');
